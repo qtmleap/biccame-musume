c590edf9c0e040ccae7a452def100dec718bb6e0
tkgstrator <nasawake.am@gmail.com>
2025-12-17 00:38:04 +0000

    feat(routes): add "About" page route and update footer with link

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,24 +1,18 @@
+import { Link } from '@tanstack/react-router'
+import dayjs from 'dayjs'
+
 /**
  * 共通フッターコンポーネント
  */
 export const Footer = () => {
   return (
-    <footer className='mt-6 text-center text-xs text-gray-500 px-4 py-4 border-t border-gray-200'>
-      <div className='container mx-auto space-y-1'>
-        <p>このサイトはビックカメラ及びビッカメ娘の非公式ファンサイトです</p>
-        <p>キャラクターの著作権は株式会社ビックカメラに帰属します</p>
-        <p>
-          <a
-            href='https://biccame.jp/guideline/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-[#e50012] hover:underline'
-          >
-            キャラクター使用のガイドライン
-          </a>
-          に基づき運営しています
-        </p>
-        <div className='flex flex-wrap justify-center gap-2 pt-1'>
+    <footer className='text-center text-xs text-gray-500 px-4 py-4 border-t border-gray-200 bg-gray-50'>
+      <div className='container mx-auto space-y-2'>
+        <div className='flex flex-wrap justify-center gap-3'>
+          <Link to='/about' className='text-gray-600 hover:text-[#e50012] transition-colors'>
+            サイトについて
+          </Link>
+          <span className='text-gray-400'>|</span>
           <a
             href='https://biccame.jp/'
             target='_blank'
@@ -34,11 +28,11 @@ export const Footer = () => {
             rel='noopener noreferrer'
             className='text-gray-600 hover:text-[#e50012] transition-colors'
           >
-            公式X (Twitter)
+            公式X
           </a>
         </div>
-        <p className='text-gray-400 pt-1'>
-          © {new Date().getFullYear()} ビッカメ娘非公式ファンサイト
+        <p className='text-gray-400'>
+          © {dayjs().year()} QuantumLeap
         </p>
       </div>
     </footer>
