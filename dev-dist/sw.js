99e2d0d310f6da705d49f4f7c71f7d795830b6ae
tkgstrator <nasawake.am@gmail.com>
2026-03-27 19:10:49 +0000

    feat(data): add Takasaki store and character profile data
    Introduces profile, store, and character information for Takasaki (高崎東口店/たかさきたん), including HTML cache, birthday, and map data. Updates localization, schemas, and package dependencies to support the new store and character.
    
    Enables display and linking of Takasaki in the application.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.vteaeegl8js"
+    "revision": "0.9jo1mujc004"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
