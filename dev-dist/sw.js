8ce5b20a8f019031ce67890b155507252dd5c964
tkgstrator <nasawake.am@gmail.com>
2026-03-27 19:20:38 +0000

    ci(github): update actions versions and add prisma config
    Upgrades GitHub Actions to latest versions for checkout, cache, and semver-compare
    to improve reliability and security. Adds initial Prisma configuration file for
    early access features and local development, and removes explicit database URL
    from the Prisma schema in favor of config-based management.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.9jo1mujc004"
+    "revision": "0.d5osf5atk9c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
